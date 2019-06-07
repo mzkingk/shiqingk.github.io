4f2e346ca2cff7629dcb50bd9052b1dd9f3a7aa0
shiqingk <978964112@qq.com>
2019-06-07 19:14:08 +0800

    update hexo

--- push.sh
+++ push.sh
@@ -8,10 +8,10 @@ git add .
 git commit -m "update hexo"
 git push origin hexo
 
-#echo "--->clone master分支"
-#git clone -b master git://github.com/shiqingk/shiqingk.github.io.git
-#sudo cp -R shiqingk.github.io/.git/ public/.git/
-#rm -rf shiqingk.github.io
+echo "--->clone master分支"
+git clone -b master git://github.com/shiqingk/shiqingk.github.io.git
+sudo cp -R shiqingk.github.io/.git/ public/.git/
+rm -rf shiqingk.github.io
 
 echo "--->提交到master分支"
 cd public
