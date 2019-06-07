7e23de28865cefedb757d20a9b86ea4eb83e38a5
shiqingk <978964112@qq.com>
2019-06-07 19:17:16 +0800

    update hexo

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ echo "--->部署"
 hexo g
 
 echo "--->提交到hexo分支"
-git checkout hexo
+#git checkout hexo
 git pull
 git add .
 git commit -m "update hexo"
@@ -15,7 +15,7 @@ git push origin hexo
 
 echo "--->提交到master分支"
 cd public
-git checkout master
+#git checkout master
 git pull
 git add .
 git commit -m "update master"
